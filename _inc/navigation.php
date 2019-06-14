9bf0fa98a30d2bb21d6dcb43d069ba191a86b754
Tristan White <oliver.tristan@gmail.com>
2019-06-14 13:52:27 +0200

    Split header and footer into partials

---
+++ _inc/navigation.php
@@ -0,0 +1,8 @@
+<nav id="navigation" aria-label="site">
+    <ul>
+        <li><a href="/">Leaked Password <span class="vh">Home</span></a></li>
+        <li><a href="/">Password</a></li>
+        <li><a href="/documentation">API</a></li>
+        <li><a href="https://github.com/triss90/leakedpassword.com" target="_blank">GitHub</a></li>
+    </ul>
+</nav>
