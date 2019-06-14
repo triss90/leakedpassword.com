b20dfe713657257108ba143fce0eaf77284e9377
Tristan White <oliver.tristan@gmail.com>
2019-06-14 16:50:18 +0200

    Implementing site-wide navigation

--- _inc/navigation.php
+++ _inc/navigation.php
@@ -1,6 +1,6 @@
 <nav id="navigation" aria-label="site">
     <ul>
-        <li><a href="/">Leaked Password <span class="vh">Home</span></a></li>
+        <li><a href="/">Leaked<span class="highlight">Password</span> <span class="vh">Home</span></a></li>
         <li><a href="/">Password</a></li>
         <li><a href="/documentation">API</a></li>
         <li><a href="https://github.com/triss90/leakedpassword.com" target="_blank">GitHub</a></li>
