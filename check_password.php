ad4307a8bc75e1db20c94a1f1c437dc0615951a9
Tristan White <oliver.tristan@gmail.com>
2019-06-14 17:01:54 +0200

    Fix site link in API call

--- check_password.php
+++ check_password.php
@@ -3,7 +3,7 @@
 $submitted_password = $_POST['password'];
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://mac.local:5757/api/password/".$submitted_password);
+curl_setopt($ch, CURLOPT_URL, "http://leakedpassword.com/api/password/".$submitted_password);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
 curl_close($ch);
