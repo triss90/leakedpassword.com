ef195b77b687b902ea6ec36e0e07a247cd6782dc
Tristan White <oliver.tristan@gmail.com>
2019-06-14 09:55:30 +0200

    Add baseline colors

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+
+<nav id="navigation" aria-label="site">
+    <ul>
+        <li><a href="/">Leaked Password <span class="vh">Home</span></a></li>
+        <li><a href="/">Test Password</a></li>
+        <li><a href="/documentation">API</a></li>
+    </ul>
+</nav>
+
+</body>
+</html>
