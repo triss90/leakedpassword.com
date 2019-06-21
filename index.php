8cd6c8b93f405d471c3eea00d896fbe2dcda190f
Tristan White <oliver.tristan@gmail.com>
2019-06-21 12:38:30 +0200

    Add bitcoin link

--- index.php
+++ index.php
@@ -43,6 +43,7 @@
 <footer class="home">
     <p>Designed, built, and maintained by <a href="https://triss.dev" target="_blank">Tristan White</a>.</p>
     <p>Powered by <a href="haveibeenpwned.com" target="_blank">Have I Been Pwned</a>.</p>
+    <p>BTC: <a href="bitcoin:3F15Wh3nyhsnze5Gg5FkQXyDhWEr4bh8sH">3F15Wh3nyhsnze5Gg5FkQXyDhWEr4bh8sH</a></p>
 </footer>
 
 <?php include('_inc/footer.php'); ?>
