1aff84b337fa1b471981e7b528afee966d71ea2f
Tristan White <oliver.tristan@gmail.com>
2019-06-21 12:38:03 +0200

    Add bitcoin link

--- _inc/credits.php
+++ _inc/credits.php
@@ -1,4 +1,5 @@
 <footer>
     <p>Designed, built, and maintained by <a href="https://triss.dev" target="_blank">Tristan White</a>.</p>
     <p>Powered by <a href="haveibeenpwned.com" target="_blank">Have I Been Pwned</a>.</p>
+    <p>BTC: <a href="bitcoin:3F15Wh3nyhsnze5Gg5FkQXyDhWEr4bh8sH">3F15Wh3nyhsnze5Gg5FkQXyDhWEr4bh8sH</a></p>
 </footer>
