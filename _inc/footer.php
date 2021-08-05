84e24d4885aa21a85855efbe11f7761e560a4641
tristanwhite <oliver.tristan@gmail.com>
2021-08-05 06:36:53 +0000

    Update menu shaodw

--- _inc/footer.php
+++ _inc/footer.php
@@ -13,8 +13,9 @@
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'UA-62496570-6');
 </script>
+<style>#carbonads *{margin:initial;padding:initial}#carbonads{font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Oxygen-Sans,Ubuntu,Cantarell,'Helvetica Neue',Helvetica,Arial,sans-serif}#carbonads{display:flex;max-width:330px;background-color:#0f151f;border:1px solid #ffcb36;z-index:100;position:fixed;right:1rem;bottom:1rem}#carbonads a{color:inherit;text-decoration:none}#carbonads a:hover{color:inherit}#carbonads span{position:relative;display:block;overflow:hidden}#carbonads .carbon-wrap{display:flex}#carbonads .carbon-wrap a{border-bottom:0}#carbonads .carbon-img{display:block;margin:0;line-height:1}#carbonads .carbon-img img{display:block}#carbonads .carbon-text{font-size:13px;padding:10px;margin-bottom:16px;line-height:1.5;text-align:left;color:#fff}#carbonads .carbon-poweredby{display:block;padding:6px 8px;background:#ffcb36;color:#0f151f;text-align:center;text-transform:uppercase;letter-spacing:.5px;font-weight:600;font-size:8px;line-height:1;border-top-left-radius:0;position:absolute;bottom:0;right:0;border-bottom:0}</style>
+<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CESI4K3M&placement=webboxdev" id="_carbonads_js"></script>
 </body>
 </html>
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+open http://localhost:8888
+php -S localhost:8888
